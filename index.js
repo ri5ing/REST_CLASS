51df0d072107e80e3b62342d11fc5768c069437e
ri5ing <uday.patil.st@gmail.com>
2026-06-27 07:07:49 +0530

    Implemented GET

--- index.js
+++ index.js
@@ -8,11 +8,27 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine" ,  "ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname,"views")));
+app.use(express.static(path.join(__dirname,"views")));
+app.use(express.static(path.join(__dirname, 'public')));
 
+let posts = [
+    {
+        username: "ri5ing",
+        content: "i love coding"
+    },
+    {
+        username: "user1",
+        content: "i am moneymagnet"
+        
+    },
+       {
+        username: "user2",
+        content: "consistency"
+    },
+]
 
-app.get("/",(req,res)=>{
-    res.send("Server Working well!");
+app.get("/post",(req,res)=>{
+    res.render("index.ejs" , {posts});
 });
 
 app.listen(port,()=> {
